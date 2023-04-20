d60125bb397610a165a8f63271e0d03105148fa3
f4rukyldrm <faruk-yildirim@outlook.com.tr>
2023-04-20 13:21:48 +0300

    first commit

---
+++ src/context/context.js
@@ -0,0 +1,40 @@
+import { useContext, createContext, useState, useEffect } from 'react'
+import Quote from '../components/Quote';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+
+    const [quote, setQuote] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    const getQuote = async () => {
+        setLoading(true);
+
+        let quote = await fetch('https://api.themotivate365.com/stoic-quote')
+            .then(response => response.json())
+            .catch(error => console.log(error));
+
+        quote['quote'] = quote['quote'].replace('@', '');
+        if (!quote['author']) {
+            quote['author'] = 'anonymous';
+        }
+        setQuote(quote);
+
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getQuote();
+    }, []);
+
+    return <AppContext.Provider value={{ quote, getQuote, loading }}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
+
+export { AppProvider };
